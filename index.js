84f35c1d7b184a2aa9cc38a10294ef03bec9badd
braintech <120840671+bbrainttech@users.noreply.github.com>
2023-07-06 22:44:05 +0100

    finished about, services and  f.a.q

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ AOS.init({
     duration: 600,
     mirror: false
 })
+
+const allFaq = q('#faq ul li .que', true)
+
+listen('mousedown', allFaq, function () {
+    this.classList.contains('act') ? cls(this, 'act', 'r') :
+        (() => {
+            allFaq.forEach(faq => { cls(faq, 'act', 'r') });
+            cls(this, 'act', 'a')
+        })()
+}, true)
